3c62f802e89a4059ce4df22fa55c2fd54e00c55a
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-09-16 10:28:47 +0000

    commit

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,8 +1,11 @@
 console.log("hello")
-const accountId=10;
+const accountId=20;
 let accountName;
 var accountPass;
 accountCity="hyderabad";
 accountName="abc";
 accountPass="123";
+accountCity="delhi";
+/* but we are preferred not to use var
+because of issue in block scope */
 console.table([accountId,accountName,accountPass,accountCity]);
