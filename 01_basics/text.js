76ed2c57cf8f0447df75dfd51c6b56e3b4782bfa
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-09-16 10:17:53 +0000

    commit

--- 01_basics/text.js
+++ 01_basics/text.js
@@ -1,1 +1,8 @@
 console.log("hello")
+const accountId=10;
+let accountName;
+var accountPass;
+accountCity="hyderabad";
+accountName="abc";
+accountPass="123";
+console.table([accountId,accountName,accountPass,accountCity]);
