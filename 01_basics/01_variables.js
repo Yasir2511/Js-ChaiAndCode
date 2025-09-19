1645b1f848f44d019415946f03fe2ea7d357f2f5
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-09-19 20:08:29 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=20;
+let accountName;
+var accountPass;
+accountCity="hyderabad";
+accountName="abc";
+accountPass="123";
+accountCity="delhi";
+/* but we are preferred not to use var
+because of issue in block scope */
+console.table([accountId,accountName,accountPass,accountCity]);
