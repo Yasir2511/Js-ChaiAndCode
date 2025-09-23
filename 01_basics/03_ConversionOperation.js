6f25a7f59fee6522c3ec3cad39c71ca07a8ca8fc
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-09-23 22:52:20 +0000

    operation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,11 +1,11 @@
 let score=true
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valInNumber=Number(score)
 
-console.log(typeof valInNumber)
-console.log(valInNumber)
+// console.log(typeof valInNumber)
+// console.log(valInNumber)
 
 //"33" =>33
 //"33abc"=>NaN
@@ -14,8 +14,28 @@ console.log(valInNumber)
 let isLoggedIn=1;
 
 let BooleanIsLogged=Boolean(isLoggedIn);
-console.log(BooleanIsLogged)
+// console.log(BooleanIsLogged)
 
 //1=>true 0=>false
 //""=>false
 // "hitesh"=>true
+
+/*******operations*******/
+let value=4
+let negvalue=-value;
+console.log(negvalue);
+
+let str="hii"
+let str1=" yasir"
+let str2=str+str1
+console.log(str2)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true)
+console.log(+true)
+
+
