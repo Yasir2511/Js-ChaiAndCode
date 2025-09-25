dbc896a3da256cf5af839ad9ae40f0cdb6ee786f
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-09-25 17:42:05 +0000

    datatypesummary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,31 @@
+//primitive datatype
+//7 types-string,number,Boolean,null,undefined,symbol,BigInt
+
+const score=76.3//number
+const name="abc"//string
+const num=false//Boolean
+const number=null//null
+let undefinedNumber//Undefined
+
+const Id=Symbol('123')//symbol
+const anotherId=Symbol('123')//symbol
+
+console.log(Id===anotherId)//check it is equal or not
+
+const BigNumber=32736893826n//BigInt
+
+//non primitive(reference) datatype
+//arrays,objects,functions
+
+const Arr=["abs","abc","abv"];
+let myObj={
+    name:"yaser",
+    age:22,
+}
+ let myFunction=function(){
+    console.log("hello");
+    
+ }
+ console.log(typeof myFunction)
+ console.log(typeof myObj)
+ console.log(typeof Arr)
