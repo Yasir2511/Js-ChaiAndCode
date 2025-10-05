1a5e74bee95ff82984b4c84519d66f45d5450df1
Yaser Viqar <149509364+Yasir2511@users.noreply.github.com>
2025-10-05 20:08:15 +0000

    commit added

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,3 +3,21 @@ const sirname="chaudhary"
 //console.log(name+sirname)//not advisable to used
 
 console.log(`hello my firstname is ${name} and sir_name is ${sirname}`)
+
+const gamename=new String('yaser-viqar  ')
+
+console.log(gamename[0])
+console.log(gamename.__proto__)
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(0))
+console.log(gamename.indexOf('a'))
+
+let sub=gamename.substring(2,7)
+console.log(sub)
+console.log(gamename.slice(1,3))
+console.log(gamename.trim())
+console.log(gamename.replace('-',' '))
+
+console.log(gamename.includes('v'))
+console.log(gamename.split('-'))
